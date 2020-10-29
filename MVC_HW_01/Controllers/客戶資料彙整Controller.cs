62b7515d7ba58bc846b61adecf605bc6220ddeaf
Dell Hsu <delldell90114@gmail.com>
2020-10-29 20:40:52 +0800

    將View加入edmx, 並產生List頁面

---
+++ MVC_HW_01/Controllers/客戶資料彙整Controller.cs
@@ -0,0 +1,22 @@
+﻿using MVC_HW_01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_HW_01.Controllers
+{
+    public class 客戶資料彙整Controller : Controller
+    {
+        private CustomerInformationEntities db = new CustomerInformationEntities();
+
+        // GET: 客戶資料彙整
+        public ActionResult Index()
+        {
+            var model = db.Get客戶資料彙整;
+
+            return View(model);
+        }
+    }
+}
--- MVC_HW_01/Models/CustomerInformation.Context.cs
+++ MVC_HW_01/Models/CustomerInformation.Context.cs
@@ -28,5 +28,6 @@ namespace MVC_HW_01.Models
         public virtual DbSet<客戶資料> 客戶資料 { get; set; }
         public virtual DbSet<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual DbSet<客戶聯絡人> 客戶聯絡人 { get; set; }
+        public virtual DbSet<Get客戶資料彙整> Get客戶資料彙整 { get; set; }
     }
 }
