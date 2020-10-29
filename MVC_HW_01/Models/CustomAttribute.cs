321345212c2e9cc350bbfe07e814f8b097c4b8af
Dell Hsu <delldell90114@gmail.com>
2020-10-29 09:51:40 +0800

    1. 建立EF Model/ Controller/ View 2. 建立自訂驗證 3. 建立partial model儲存驗證 4. 初步建立IRepository

---
+++ MVC_HW_01/Models/CustomAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace MVC_HW_01.Models
+{
+    public class PhoneFormatAttribute : DataTypeAttribute 
+    {
+        Regex regex = new Regex(@"\d{4}-\d{6}");
+
+        public PhoneFormatAttribute() : base(DataType.Text)
+        {
+            ErrorMessage = "手機格式錯誤！ e.g: 0912-345678";
+        }
+
+        public override bool IsValid(object value)
+        {
+            if(value == null)
+            {
+                return true;
+            }
+
+            return regex.IsMatch(Convert.ToString(value));
+        }
+    }
+}
